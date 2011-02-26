fac8a5be066262b3d2ec2e9f5e8ba98d6600a6d7
dstufft <donald.stufft@gmail.com>
2011-02-26 02:06:57 -0500

    update directory to use media instead of static

--- staticfiles_mediafinder/finders.py
+++ staticfiles_mediafinder/finders.py
@@ -2,7 +2,7 @@ from django.contrib.staticfiles.storage import AppStaticStorage as _DjangoAppSta
 from django.contrib.staticfiles.finders import AppDirectoriesFinder as _DjangoAppDirectoriesFinder
 
 class AppStaticStorage(_DjangoAppStaticStorage):
-    source_dir = 'static'
+    source_dir = 'media'
 
 class AppDirectoriesFinder(_DjangoAppDirectoriesFinder):
     storage_class = AppStaticStorage
